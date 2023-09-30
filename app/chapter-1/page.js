3b4b2458afb9cfdbeb060d3b601e5d6f31bf3607
Rajvi Patel <136672895+rajvi1163@users.noreply.github.com>
2023-09-30 19:07:35 +0530

    Update page.js

--- app/chapter-1/page.js
+++ app/chapter-1/page.js
@@ -24,7 +24,7 @@ const page = () => {
         </div>
     </div>
     <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
-    <iframe width="887" height="499" src="https://www.youtube.com/embed/VYlTI0RuPlI" title="animation 1 final" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="887" height="499" src="https://www.youtube.com/embed/_lNi9mEyLD8" title="animation 1 final" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
     </>
   )
